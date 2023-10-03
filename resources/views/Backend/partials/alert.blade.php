29c85f249f27f6d3ab513a2b2d93a7fa1c4b185c
JB <dev.jobaed@gmail.com>
2023-10-03 17:42:23 +0600

    Added Alert And Some Frontend Issue

--- resources/views/Backend/partials/alert.blade.php
+++ resources/views/Backend/partials/alert.blade.php
@@ -1,25 +1,28 @@
 @if ($message = Session::get('alert-success'))
-    <div class="alert alert-success d-flex align-items-center alert-dismissible" role="alert">
-        <span class="lnr lnr-thumbs-up"></span>&nbsp;
-        <div>
-            {{ $message }}
-        </div>
-        <button type="button" class="btn-close" data-coreui-dismiss="alert" aria-label="Close"></button>
+    <div class="alert alert-success alert-dismissible text-white" role="alert">
+                  <span class="text-sm">
+                    {{ $message }}
+                  </span>
+        <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
     </div>
 @elseif($message = Session::get('alert-error'))
-    <div class="alert alert-danger d-flex align-items-center alert-dismissible" role="alert">
-        <span class="lnr lnr-warning"></span>&nbsp;
-        <div>
-            {{ $message }}
-        </div>
-        <button type="button" class="btn-close" data-coreui-dismiss="alert" aria-label="Close"></button>
+    <div class="alert alert-danger alert-dismissible text-white" role="alert">
+                  <span class="text-sm">
+                    {{ $message }}
+                  </span>
+        <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
     </div>
 @elseif($message = Session::get('alert-warning'))
-    <div class="alert alert-warning d-flex align-items-center alert-dismissible" role="alert">
-        <span class="lnr lnr-warning"></span>&nbsp;
-        <div>
-            {{ $message }}
-        </div>
-        <button type="button" class="btn-close" data-coreui-dismiss="alert" aria-label="Close"></button>
+    <div class="alert alert-warning alert-dismissible text-white" role="alert">
+                  <span class="text-sm">
+                    {{ $message }}
+                  </span>
+        <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
     </div>
 @endif
--- resources/views/Backend/partials/table.blade.php
+++ resources/views/Backend/partials/table.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-12">
             <div class="card my-4">
-                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2" style="z-index: 0 !important;">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
                         <h6 class="text-white text-capitalize ps-3">Authors table</h6>
                     </div>
@@ -202,7 +202,7 @@
     <div class="row">
         <div class="col-12">
             <div class="card my-4">
-                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2" style="z-index: 0 !important;">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
                         <h6 class="text-white text-capitalize ps-3">Projects table</h6>
                     </div>
